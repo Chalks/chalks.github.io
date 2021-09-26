b4ff58d4d586adfcbb2db013679b5bdef7520d76
Jonathan Walters <skychalk@gmail.com>
2021-09-26 18:10:43 -0400

    basic intro content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="prose">
+    <div class="prose mx-auto">
         <h1>Hi, I'm Jonathan Walters</h1>
+
+        <p>I build teams that deliver fantastic product. Do you need some help with that? <a href="/contact">Contact me</a>. When I'm not running a dev team I'm building frontend. This website is built entirely without any backend architecture using:</p>
+
+        <ul>
+            <li><a href="https://vuejs.org/">Vue</a> - It's fine. At least it's not Angular?</li>
+            <li><a href="https://nuxtjs.org/">NuxtJS</a> - Something has to generate static files</li>
+            <li><a href="https://tailwindcss.com/">Tailwind CSS</a> - CSS is dead, long live Tailwind</li>
+            <li><a href="https://pages.github.com/">Github Pages</a> - I dunno, I didn't feel like creating a Netlify account</li>
+            <li><a href="https://formcake.com">Formcake</a> - Easy form backend (I made this)</li>
+            <li><a href="https://github.com/Chalks/chalks.github.io">Here's the source code</a></li>
+        </ul>
     </div>
 </template>
