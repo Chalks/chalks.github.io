b3bd728d6ce65aab77fa84e62721d8abb63603d1
Jonathan Walters <skychalk@gmail.com>
2021-12-11 01:31:09 -0500

    make wip more wippy

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="prose">
-        WIP
+    <div class="wip">
+        <div class="wipper">WIP</div>
     </div>
 </template>
+
+<style>
+    .wip {
+        @apply flex absolute inset-0 items-center justify-center;
+        font-family: 'Cousine', monospace;
+        font-size: 20px;
+        line-height: 0;
+        font-weight: 600;
+    }
+</style>
