217272f66ece1af978a5094301f1578e2f0b3d35
Jonathan Walters <skychalk@gmail.com>
2021-12-11 23:55:57 -0500

    simple error layout

---
+++ src/components/ErrorForm.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            message: '',
+        };
+    },
+
+    head: {
+        script: [
+            {
+                src: "https://www.google.com/recaptcha/api.js",
+            },
+        ],
+    },
+
+    mounted() {
+        window.recaptchaJank = this.onSubmit;
+    },
+
+    methods: {
+        onSubmit(token) {
+            this.$refs.errorForm.submit();
+        },
+
+        focus() {
+            this.$refs.emailField.focus();
+        },
+    }
+}
+</script>
+<template>
+    <form
+        ref="errorForm"
+        class="prose mx-auto"
+        action="https://api.formcake.com/api/form/a5866167-1e7d-4aed-9898-2436a78b44df/submission"
+        method="post"
+    >
+        <h3>Whoops, something broke</h3>
+        <p>Let me know what happened please!</p>
+
+        <input type="hidden" name="honey" />
+
+        <input
+            ref="emailField"
+            v-model="email"
+            type="text"
+            name="email"
+            placeholder="Your email address"
+        />
+
+        <textarea
+            v-model="message"
+            name="message"
+            placeholder="Describe what happened!"
+        />
+
+        <button
+            class="g-recaptcha submit"
+            data-sitekey="6Lf6IyUaAAAAAPTTe__sC4052VzvGcwFlBHeajA0"
+            data-callback='recaptchaJank'
+            data-action='submit'
+        >
+            Report Error
+        </button>
+    </form>
+</template>
+
---
+++ src/layouts/error.vue
@@ -0,0 +1,26 @@
+<script>
+import Footer from '~/components/Footer.vue';
+import Header from '~/components/Header.vue';
+import ErrorForm from '~/components/ErrorForm.vue';
+
+export default {
+    components: {
+        Footer,
+        Header,
+        ErrorForm,
+    },
+};
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen">
+        <Header />
+
+        <div class="container mx-auto flex-grow my-8">
+            <ErrorForm />
+        </div>
+
+        <Footer />
+    </div>
+</template>
+
