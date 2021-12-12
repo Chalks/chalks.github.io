66f7bdcbd6fb38b2582ef2908a2fc3d9b8124b65
Jonathan Walters <skychalk@gmail.com>
2021-12-12 13:13:07 -0500

    update readme and index

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Personal Website
+
+This is built with Vue, Nuxt, Tailwind, and Formcake. It's hosted on Netlify and you can view the site at [jdw.me](https://jdw.me).
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,7 +23,7 @@ export default {
             <li><a href="https://vuejs.org/">Vue</a> - Just playing with different frameworks, branching out from React</li>
             <li><a href="https://nuxtjs.org/">NuxtJS</a> - Something has to generate static files</li>
             <li><a href="https://tailwindcss.com/">Tailwind CSS</a> - CSS is dead, long live Tailwind</li>
-            <li><a href="https://pages.github.com/">Github Pages</a> - Simpler than Netlify?</li>
+            <li><a href="https://www.netlify.com/">Netlify</a> - Fast and free static hosting</li>
             <li><a href="https://formcake.com">Formcake</a> - Easy form backend (I made this!)</li>
             <li><a href="https://github.com/Chalks/chalks.github.io">This page's source code</a> - I made this too</li>
         </ul>
