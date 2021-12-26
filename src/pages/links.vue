488d280da9e7b7d6cef5ab34f751943692057219
Jonathan Walters <skychalk@gmail.com>
2021-12-26 15:46:52 -0500

    resume

--- src/pages/links.vue
+++ src/pages/links.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="flex justify-center">
         <div class="pillar">
-            <p class="pillar-word">games</p>
+            <h2 class="pillar-word">games</h2>
             <a href="https://lichess.org" target="_blank" class="pillar-word">LICHESS.ORG</a>
             <a href="https://alt.org/nethack/" target="_blank" class="pillar-word">NETHACK</a>
             <a href="https://tagpro.gg" target="_blank" class="pillar-word">TAGPRO.GG</a>
             <a href="https://teamwood.itch.io/super-auto-pets" target="_blank" class="pillar-word">SUPER.AUTO.PETS</a>
-            <p class="pillar-word">garbage</p>
+
+            <h2 class="pillar-word">garbage</h2>
             <a href="https://youtube.com/feed/subscriptions" target="_blank" class="pillar-word">YOUTUBE.COM</a>
             <a href="https://old.reddit.com" target="_blank" class="pillar-word">REDDIT.COM</a>
             <a href="https://facebook.com" target="_blank" class="pillar-word">FACEBOOK.COM</a>
--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -1,15 +1,46 @@
 <template>
-    <div class="pillar bg-red-300">
-        <div class="wipper">WIP</div>
+    <div class="flex justify-around flex-wrap">
+        <div class="pillar wide">
+            <h1 class="pillar-word">Jonathan Walters</h1>
+
+            <h2 class="pillar-word">Work History:</h2>
+            <p class="pillar-word">Stealth Startup 2021-…, Founding Engineer</p>
+            <p class="pillar-word">Formcake 2021-…, Founder</p>
+            <p class="pillar-word">Nokia (Deepfield) 2016-2021, Software Engineering Lead</p>
+            <p class="pillar-word">A3 Creative 2015-2016, Lead Developer & Project Manager</p>
+            <p class="pillar-word">Booz Allen Hamilton 2011-2015 Senior Consultant</p>
+
+            <h2 class="pillar-word">Education:</h2>
+            <p class="pillar-word">Purdue University B.S. Computer Science 2011</p>
+
+            <h2 class="pillar-word">Proficient:</h2>
+            <p class="pillar-word">Agile</p>
+            <p class="pillar-word">Javascript (ES6, React, Vue)</p>
+            <p class="pillar-word">HTML</p>
+            <p class="pillar-word">CSS</p>
+            <p class="pillar-word">MySQL</p>
+
+            <h2 class="pillar-word">Familiar:</h2>
+            <p class="pillar-word">Python</p>
+            <p class="pillar-word">PHP</p>
+            <p class="pillar-word">Java</p>
+            <p class="pillar-word">Ruby</p>
+            <p class="pillar-word">C</p>
+            <p class="pillar-word">C++</p>
+        </div>
     </div>
 </template>
 
 <style>
-    .wip {
-        @apply flex absolute inset-0 items-center justify-center;
-        font-family: 'Cousine', monospace;
-        font-size: 20px;
-        line-height: 0;
-        font-weight: 600;
+    h1.pillar-word {
+        @apply bg-yellow-900 text-gray-50;
+    }
+
+    h2.pillar-word {
+        @apply bg-blue-400 text-gray-50;
+    }
+
+    p.pillar-word {
+        @apply bg-gray-400 text-gray-50;
     }
 </style>
