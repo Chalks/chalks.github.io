8976c921a5aff362c19f78195a3db8d2cdcc372c
Jonathan Walters <skychalk@gmail.com>
2021-12-30 13:53:52 -0500

    fixup thanks page

--- src/pages/thanks.vue
+++ src/pages/thanks.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="prose mx-auto prose-red">
+    <div class="prose my-12 mx-auto prose-red">
         <h1>Thanks for Contacting Me</h1>
 
         <p>With the power of <a href="https://formcake.com">Formcake</a>, I got a chat notification, an email, and a Jira ticket that all tell me to respond to you. Hopefully I didn't ignore all of that.</p>
