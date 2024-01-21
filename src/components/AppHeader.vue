a49fdba76d31211bbc7f7e6b1acb11dcd9057d88
Jonathan Walters <skychalk@gmail.com>
2024-01-21 13:22:05 -0500

    feat: update dependencies and header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,89 +1,61 @@
+<script setup>
+import {Icon} from '@iconify/vue';
+</script>
+
 <template>
-    <div class="trigger">
-        <div class="header top-0 left-0 md:top-4 md:left-4">
-            <NuxtLink to="/"><img src="/favicon-96x96.png" /></NuxtLink>
-            <NuxtLink to="/resume">Resume</NuxtLink>
-            <NuxtLink to="/links">Links</NuxtLink>
-        </div>
+    <div
+        class="
+            relative
+            md:fixed
+            top-0
+            bg-gray-700
+            flex
+            flex-row
+            md:flex-col
+            gap-4
+            items-center
+            pl-4
+            mb-2
+            md:pl-0
+            md:pt-4
+            text-white
+            rounded-bl-full
+            rounded-tr-full
+            rounded-br-full
+            overflow-hidden
+            h-12
+            w-fit
+            md:w-12
+            md:h-auto
+        "
+    >
+        <NuxtLink to="/resume" class="hover:text-blue-300 transition-colors">
+            <Icon icon="tabler:file-cv" />
+        </NuxtLink>
+
+        <NuxtLink to="/links" class="hover:text-blue-300 transition-colors">
+            <Icon icon="carbon:network-2" />
+        </NuxtLink>
+
+        <NuxtLink
+            to="/"
+            class="
+                rounded-full
+                h-full
+                w-12
+                md:h-auto
+                md:w-full
+                overflow-hidden
+                border-4
+                border-gray-700
+                hover:animate-spin
+                transition-colors
+            "
+        >
+            <img src="/favicon-96x96.png" class="object-cover" />
+        </NuxtLink>
     </div>
 </template>
 
 <style scoped>
-    .trigger {
-        @apply fixed top-0 left-0 z-50;
-        height: calc(1rem + 36px);
-        width: calc(1rem + 36px + 50px);
-    }
-
-    .trigger:hover .header {
-        animation: rotate 0.2s ease forwards;
-    }
-
-    .trigger:hover img {
-        animation: reverseRotate 0.2s ease forwards;
-    }
-
-    @media (hover: none) {
-        .trigger {
-            @apply relative;
-            top: -100%;
-        }
-
-        .header {
-            right: 0;
-            left: unset !important;
-            transform: rotate(0) !important;
-        }
-
-        img {
-            transform: rotate(0) !important;
-        }
-    }
-
-    .header {
-        @apply
-            fixed
-            bg-gray-700
-            z-50
-            space-x-4
-            transition-colors
-            rounded-l-full
-            flex
-            items-center;
-
-        padding: 2px;
-        padding-right: 1rem;
-
-        transform: rotate(-90deg);
-        transform-origin: 18px 50%;
-    }
-
-    img {
-        @apply rounded-full;
-        width: 32px;
-        height: 32px;
-        transform-origin: 50% 50%;
-        margin-right: 1px;
-        transform: rotate(90deg);
-    }
-
-    a {
-        @apply text-gray-200 transition-colors;
-
-        &:hover {
-            @apply text-gray-50;
-        }
-    }
-
-    @keyframes rotate {
-        0% { transform: rotate(-90deg); }
-        30% { transform: rotate(-90deg); }
-        100% { transform: rotate(0); }
-    }
-
-    @keyframes reverseRotate {
-        0% { transform: rotate(90deg); }
-        30% { transform: rotate(90deg); }
-        100% { transform: rotate(0); }
-    }
 </style>
