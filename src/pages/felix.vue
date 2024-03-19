1680bbb7007563a4806ec7a4e59ca1e59464572a
Jonathan Walters <skychalk@gmail.com>
2024-03-19 09:05:32 -0400

    feature: add more links for felix

--- src/pages/felix.vue
+++ src/pages/felix.vue
@@ -5,11 +5,11 @@ seo({
 </script>
 
 <template>
-    <div class="flex flex-col gap-96 justify-center items-center py-8">
-        <div class="flex items-center justify-center gap-96">
+    <div class="max-w-prose w-full flex mx-auto py-8">
+        <div class="flex flex-wrap gap-tile">
             <a
                 href="https://youtube.com"
-                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
                 target="_blank"
             >
                 <img src="/kiddos/youtube.png" class="object-contain group-hover:animate-wiggle" />
@@ -17,31 +17,38 @@ seo({
 
             <a
                 href="https://www.roblox.com/discover"
-                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
                 target="_blank"
             >
                 <img src="/kiddos/roblox.png" class="object-contain group-hover:animate-wiggle" />
             </a>
+
             <a
                 href="https://www.roblox.com/games/262014008/Movie-Maker-3-OPEN-Beta"
-                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
                 target="_blank"
             >
                 <img src="/kiddos/robloxmm3.png" class="object-cover group-hover:animate-wiggle" />
             </a>
-        </div>
-        <div class="flex items-center justify-center gap-96">
+
             <a
                 href="https://scratch.mit.edu/"
-                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
                 target="_blank"
             >
                 <img src="/kiddos/scratch.png" class="object-contain group-hover:animate-wiggle" />
             </a>
 
             <a
+                href="https://musiclab.chromeexperiments.com/"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
+            >
+                <img src="/kiddos/musicbox.png" class="object-contain group-hover:animate-wiggle" />
+            </a>
+
+            <a
                 href="steam://steam"
-                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                class="rounded-lg border bg-white prose-tile flex items-center justify-center p-1 shadow hover:shadow-lg group"
             >
                 <img src="/kiddos/steam.png" class="object-contain group-hover:animate-wiggle" />
             </a>
--- src/pages/josephine.vue
+++ src/pages/josephine.vue
@@ -5,22 +5,24 @@ seo({
 </script>
 
 <template>
-    <div class="flex flex-col gap-8 justify-center items-center py-8">
-        <a
-            href="https://youtube.com"
-            class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
-            target="_blank"
-        >
-            <img src="/kiddos/youtube.png" class="object-contain group-hover:animate-wiggle" />
-        </a>
+    <div class="max-w-prose w-full flex mx-auto py-8">
+        <div class="flex flex-wrap gap-tile">
+            <a
+                href="https://youtube.com"
+                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                target="_blank"
+            >
+                <img src="/kiddos/youtube.png" class="object-contain group-hover:animate-wiggle" />
+            </a>
 
-        <a
-            href="https://www.roblox.com/discover"
-            class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
-            target="_blank"
-        >
-            <img src="/kiddos/roblox.png" class="object-contain group-hover:animate-wiggle" />
-        </a>
+            <a
+                href="https://www.roblox.com/discover"
+                class="rounded-lg border bg-white w-24 h-24 flex items-center justify-center p-1 shadow hover:shadow-lg group"
+                target="_blank"
+            >
+                <img src="/kiddos/roblox.png" class="object-contain group-hover:animate-wiggle" />
+            </a>
+        </div>
     </div>
 </template>
 
